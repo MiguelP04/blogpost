21bbd947986abc3f51f1c5e2f1800828ba0741cb
MiguelP04 <miguel09112004@gmail.com>
2025-09-15 11:40:30 -0400

    KEY-3: implement authentication login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="flex flex-col items-center gap-6 h-80 w-90 border rounded-md">
+        <div className="flex flex-col items-center">
+          <h1 className="mt-12 text-xl font-bold">Login</h1>
+          <p className="text-zinc-500">Sign in to your account</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="" className="flex flex-col">
+            Username
+            <input type="text" className="outline" />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            Password
+            <input type="text" className="outline" />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
