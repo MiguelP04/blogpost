58454eee10d3f991e4ea25e7ddba8fb85b2f3a75
MiguelP04 <94864889+MiguelP04@users.noreply.github.com>
2025-09-16 19:24:41 -0400

    KEY-2:create the login form with validation (#2)
    * KEY-2:create the login form with validation
    
    * feat: uploading services folder
    
    * KEY-2:create the login form with validation
    
    * feat: create form and form  validations

---
+++ src/hooks/useUsers.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { fetchUsers } from "../services/fetchUsers";
+
+export function useUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const data = await fetchUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUsers();
+  }, []);
+
+  return { users, loading, error };
+}
